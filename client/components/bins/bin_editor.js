5dfd4ebdb66339ab56a15f4b3cda5f84ef545a73
Eric Milas <eric-milas@idexx.com>
2020-01-07 09:16:37 -0500

    Made it mostly through the course but some components are no longer compatible so I am abandoning the final project

--- client/components/bins/bin_editor.js
+++ client/components/bins/bin_editor.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
 
 class BinEditor extends Component{
     render() {
         return(
-            <div>Bin Editor</div>
+            <div className="col-xs-8">
+                <h5>Input</h5>
+                <CodeMirror options={{mode: 'markdown', lineNumbers: 'true'}}/>
+            </div>
         );
     }
 }
